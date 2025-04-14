c246aaf9198385b8e3e590b509d2a2f632f477f2
cleaning-masters <ribcanskijb@gmail.com>
2025-04-14 22:51:47 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,27 +1,12 @@
-// Анімація при скролі
-const animatedItems = document.querySelectorAll('.fade-in, .fade-in-up');
-
-function checkVisibility() {
-  animatedItems.forEach(item => {
-    const rect = item.getBoundingClientRect();
-    if (rect.top < window.innerHeight - 100) {
-      item.classList.add('show');
-    }
-  });
-}
-
-window.addEventListener('scroll', checkVisibility);
-window.addEventListener('load', checkVisibility);
-
 // Телефон: дзвінок на мобілці, текст на ПК
-const phoneBtn = document.getElementById('phone-btn');
+const phoneBtn = document.querySelector('.floating-phone');
 phoneBtn.addEventListener('click', () => {
   if (/Mobi|Android/i.test(navigator.userAgent)) {
     // Мобільний пристрій
     window.location.href = 'tel:+380123456789';
   } else {
     // ПК
-    alert('Наш номер: +380685669590');
+    alert('Наш номер: +38 0685669590');
   }
 });
 
